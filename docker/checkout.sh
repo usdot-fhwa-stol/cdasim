2afa4d41a7a29883625304dc899522c05db841a4
fangzhou1227 <fangzhou1227@gmail.com>
2020-06-19 07:08:26 -0700

    Add files for docker build

---
+++ docker/checkout.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+#  Copyright (C) 2018-2020 LEIDOS.
+# 
+#  Licensed under the Apache License, Version 2.0 (the "License"); you may not
+#  use this file except in compliance with the License. You may obtain a copy of
+#  the License at
+# 
+#  http://www.apache.org/licenses/LICENSE-2.0
+# 
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#  License for the specific language governing permissions and limitations under
+#  the License.
+
+# CARMA packages checkout script
+# Optional argument to set the root checkout directory with no ending '/' default is '~'
+
+set -ex
+
+dir=~
+while [[ $# -gt 0 ]]; do
+      arg="$1"
+      case $arg in
+            -d|--develop)
+                  BRANCH=develop
+                  shift
+            ;;
+            -r|--root)
+                  dir=$2
+                  shift
+                  shift
+            ;;
+      esac
+done
+
+if [[ "$BRANCH" = "develop" ]]; then
+      git clone https://github.com/usdot-fhwa-stol/CARMAMsgs.git ~/src/CARMAMsgs --branch $BRANCH --depth 1
+      git clone https://github.com/usdot-fhwa-stol/CARMAUtils.git ~/src/CARMAUtils --branch $BRANCH --depth 1    
+else
+      git clone https://github.com/usdot-fhwa-stol/CARMAMsgs.git ${dir}/src/CARMAMsgs --branch CARMAMsgs_1.3.0 --depth 1
+      git clone https://github.com/usdot-fhwa-stol/CARMAUtils.git ${dir}/src/CARMAUtils --branch CARMAUtils_1.3.0 --depth 1
+fi
+
+# Required to build the ackermann_msgs and carla_msgs.
+git clone https://github.com/ros-drivers/ackermann_msgs.git ${dir}/src/ackermann_msgs --branch master --depth 1
+git clone https://github.com/carla-simulator/ros-carla-msgs.git ${dir}/src/carla_msgs --branch master --depth 1
