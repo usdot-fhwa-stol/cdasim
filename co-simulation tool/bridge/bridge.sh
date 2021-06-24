7424b5bd77946f566fb7b0b3d99868fefe8d1c96
DFCui <dfcui2015@gmail.com>
2021-06-23 23:29:41 -0400

    change python3 to python3.7 in bridge file

--- co-simulation tool/bridge/bridge.sh
+++ co-simulation tool/bridge/bridge.sh
@@ -2,6 +2,6 @@
 
 #x-terminal-emulator
 
-python3 carla_mosaic_bridge.py --bridge-server-port 8913 --map Town04 net/Town04.net.xml
+python3.7 carla_mosaic_bridge.py --bridge-server-port 8913 --map Town04 net/Town04.net.xml
 
 
