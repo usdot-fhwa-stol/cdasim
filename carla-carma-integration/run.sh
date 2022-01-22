e076f864967c0795c7fba7443709ebb6211643c4
chengyuan0124 <cheng.yuan@leidos.com>
2022-01-21 20:12:40 -0500

    Adding docker components
    - Adding build.sh for build Dockerfile
    - Adding Dockerfile for build image
    - Adding run.sh for executing docker image

---
+++ carla-carma-integration/run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+RUNTIME=""
+DOCKER_VERSION=$(docker version --format '{{.Client.Version}}' | cut --delimiter=. --fields=1,2)
+if [[ $DOCKER_VERSION < "19.03" ]] && ! type nvidia-docker; then
+    RUNTIME="--gpus all"
+else
+    RUNTIME="--runtime=nvidia"
+fi
+docker run \
+       -it --rm\
+       --name carla_carma_integration \
+       --net=host \
+       carla-carma-integration:latest
