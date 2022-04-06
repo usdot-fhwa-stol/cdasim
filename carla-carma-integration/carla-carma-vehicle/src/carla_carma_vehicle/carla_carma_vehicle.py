d9a3a81031425934be9e6db90cf552536b0b9e7b
Isaiah <isaiah.d.lewis@leidos.com>
2022-04-06 20:05:59 +0000

    Undo changes to carla_carma_vehicle.py

--- carla-carma-integration/carla-carma-vehicle/src/carla_carma_vehicle/carla_carma_vehicle.py
+++ carla-carma-integration/carla-carma-vehicle/src/carla_carma_vehicle/carla_carma_vehicle.py
@@ -62,12 +62,11 @@ class CarlaVirtualVehicle(object):
         self.player = None
         self.player_created = False
         self.sensor_actors = []
-
-        #Get vehicle parameter values
         self.actor_filter = rospy.get_param('/vehicle_model', 'vehicle.*')
         self.actor_spawnpoint = None
         self.role_name = rospy.get_param('~role_name', 'virtual_vehicle')
         self.vehicle_make = rospy.get_param('/vehicle_make',"Default")
+        self.vehicle_model = rospy.get_param('/vehicle_model', "Default_Model")
         self.vehicle_year = rospy.get_param('/vehicle_year', 1999)
         self.vehicle_length = rospy.get_param('/vehicle_length', 1.0)
         self.vehicle_width = rospy.get_param('/vehicle_width',  2.0)
