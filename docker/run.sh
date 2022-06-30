a6b8be66055b247005c6568ebe43fb99b4674586
Cheng Yuan <cheng.yuan@leidos.com>
2022-06-29 22:36:39 -0400

    Put prior version identification method back

--- docker/run.sh
+++ docker/run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 RUNTIME=""
+IMAGE="carma-xil-cosimulation"
 DOCKER_VERSION=$(docker version --format '{{.Client.Version}}' | cut --delimiter=. --fields=1,2)
 if [[ $DOCKER_VERSION < "19.03" ]] && ! type nvidia-docker; then
     RUNTIME="--gpus all"
@@ -8,6 +9,17 @@ else
     RUNTIME="--runtime=nvidia"
 fi
 
+while [[ $# -gt 0 ]]; do
+    arg="$1"
+    case $arg in
+        -v|--version)
+            COMPONENT_VERSION_STRING="$2"
+            shift
+            shift
+            ;;
+    esac
+done
+echo "##### Running usdotfhwastol/carma-xil-cosimulation:$COMPONENT_VERSION_STRING docker container #####"
 docker run \
        --rm -it\
        --gpus all\
@@ -15,4 +27,4 @@ docker run \
        -v /tmp/.X11-unix:/tmp/.X11-unix\
        -e DISPLAY=$DISPLAY\
        -e QT_X11_NO_MITSHM=1\
-       --user=carla_sumo_mosaic usdotfhwastol/carma-xil-cosimulation:1.0.0-beta
+       --user=carla_sumo_mosaic usdotfhwastol/carma-xil-cosimulation:$COMPONENT_VERSION_STRING
