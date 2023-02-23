6ed2d087e19a0ea6cc766b3848caac56e6fa87dd
Cheng Yuan <cheng.yuan@leidos.com>
2023-02-23 13:52:47 -0500

    Push evc-sumo bridge unit test

---
+++ evc-sumo/test/unit_test.py
@@ -0,0 +1,76 @@
+import unittest
+import os
+import sys
+from pathlib import Path
+
+sys.path.append('../evc_sumo_bridge')
+from evc_connector import EvcConnector
+
+class TestControllerIOGreen(object):
+    def is_cob_on(self, index):
+        green_list = [i for i in range(0, 16)]
+        if index in green_list:
+            return True
+        else:
+            return False
+
+class TestControllerIOYellow(object):
+    def is_cob_on(self, index):
+        yellow_list = [i for i in range(16, 32)]
+        if index in yellow_list:
+            return True
+        else:
+            return False
+
+class TestControllerIORed(object):
+    def is_cob_on(self, index):
+        red_list = [i for i in range(32, 48)]
+        if index in red_list:
+            return True
+        else:
+            return False
+
+class TestEvcConnector(unittest.TestCase):
+    def setUp(self):
+        self.asc3app_path = Path(os.path.abspath(os.path.join(__file__, "..", "asc3app")))
+        self.controller_cfg_path = Path(os.path.abspath(os.path.join(__file__, "..", "resources", "test_controller_cfg.json")))
+
+    def test_get_controller_cfg_path_list(self):
+        evc_connector = EvcConnector(self.asc3app_path, self.controller_cfg_path)
+        evc_connector.get_controller_cfg_path_list()
+        self.assertEqual(len(evc_connector.controller_cfg_path_list), 2)
+
+    def test_cob_to_traffic_light_status(self):
+        evc_connector = EvcConnector(self.asc3app_path, self.controller_cfg_path)
+        phase = 1
+        ## get green light string
+        green_io = TestControllerIOGreen()
+        state = evc_connector.COB_to_traffic_light_status(green_io, phase)
+        self.assertEqual(state, "g")
+        ## get yellow light string
+        yellow_io = TestControllerIOYellow()
+        state = evc_connector.COB_to_traffic_light_status(yellow_io, phase)
+        self.assertEqual(state, "y")
+        ## get red light string
+        red_io = TestControllerIORed()
+        state = evc_connector.COB_to_traffic_light_status(red_io, phase)
+        self.assertEqual(state, "r")
+
+    def test_get_traffic_light_status(self):
+        evc_connector = EvcConnector(self.asc3app_path, self.controller_cfg_path)
+
+        ## get green light state string
+        green_io = TestControllerIOGreen()
+        state = evc_connector.get_traffic_light_status_from_EVC(green_io, [{"index": [0, 1], "phaseId": 1}, {"index": [2, 3], "phaseId": 2}])
+        self.assertEqual(state, "gggg")
+        ## get yellow light state string
+        yellow_io = TestControllerIOYellow()
+        state = evc_connector.get_traffic_light_status_from_EVC(yellow_io, [{"index": [0, 1], "phaseId": 1}, {"index": [2, 3], "phaseId": 2}])
+        self.assertEqual(state, "yyyy")
+        ## get red light state string
+        red_io = TestControllerIORed()
+        state = evc_connector.get_traffic_light_status_from_EVC(red_io, [{"index": [0, 1], "phaseId": 1}, {"index": [2, 3], "phaseId": 2}])
+        self.assertEqual(state, "rrrr")
+
+if __name__ == '__main__':
+    unittest.main()
