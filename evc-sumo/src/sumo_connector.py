69403a6b385c58b415ac5fa282e25ffc0286c8b7
Cheng Yuan <cheng.yuan@leidos.com>
2023-03-02 02:02:44 -0500

    Added copyright and function to get step length from SUMO via traci

--- evc-sumo/src/sumo_connector.py
+++ evc-sumo/src/sumo_connector.py
@@ -1,3 +1,18 @@
+ #   Copyright (C) 2022 LEIDOS.
+ #
+ #   Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ #   use this file except in compliance with the License. You may obtain a copy of
+ #   the License at
+ #
+ #   http://www.apache.org/licenses/LICENSE-2.0
+ #
+ #   Unless required by applicable law or agreed to in writing, software
+ #   distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ #   WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ #   License for the specific language governing permissions and limitations under
+ #   the License.
+ #
+
 import sys
 import os
 
@@ -25,6 +40,9 @@ class SumoConnector:
         self.port = port
         self.order_num = order_num
         self.traci = traci
+
+    def traci_get_step_length(self):
+        return traci.simulation.getDeltaT()
 
     @contextmanager
     def traci_handler(self):
