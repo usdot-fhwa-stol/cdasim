dd0a194ab9de41aabe744541797d04ff19fd652d
Cheng Yuan <cheng.yuan@leidos.com>
2023-03-02 02:04:06 -0500

    Updated traci-port to 2010 which is the default setting of SUMO launched by MOSAIC

--- evc-sumo/src/evc_sumo_bridge.py
+++ evc-sumo/src/evc_sumo_bridge.py
@@ -1,3 +1,19 @@
+ #   Copyright (C) 2022 LEIDOS.
+ #
+ #   Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ #   use this file except in compliance with the License. You may obtain a copy of
+ #   the License at
+ #
+ #   http://www.apache.org/licenses/LICENSE-2.0
+ #
+ #   Unless required by applicable law or agreed to in writing, software
+ #   distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ #   WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ #   License for the specific language governing permissions and limitations under
+ #   the License.
+ #
+
+
 import sys
 import os
 from pathlib import Path
@@ -17,12 +33,12 @@ if __name__ == "__main__":
                      required=True,
                      help='asc3app-application file path')
     arg.add_argument('--traci-port',
-                     default=2000,
+                     default=2010,
                      help='Traci port to connect to SUMO')
     arg.add_argument('--traci-order-num',
                      default=2,
                      help='Traci order number for SUMO multi-clients connection')
     arg.add_argument('--evc-sumo-cfg-path',
-                     default='resources/controller_cfg.json')
+                     default='resources/evc_sumo_cfg.json')
     args = arg.parse_args()
     run(args)
