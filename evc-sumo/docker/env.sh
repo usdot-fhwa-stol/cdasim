4c3dd569ffd5e88d08da17bd43ac3541beb35ed9
Eric Chen <122404059+EricChen-Lei@users.noreply.github.com>
2023-03-23 15:10:29 -0400

    change env.sh content
    take out the not used part

--- evc-sumo/docker/env.sh
+++ evc-sumo/docker/env.sh
@@ -16,11 +16,6 @@
 
 # Set environment variables here.
 
-export NS3_HOME="/opt/ns-3/"
-export JAVA_HOME="/usr/lib/jvm/java-11-jdk-amd64"
-export SUMO_HOME="/opt/sumo/sumo-1_12_0"
-export CARLA_HOME="/opt/carla"
-export CARMA_SIMULATION_HOME="/opt/carma-simulation"
+export SUMO_HOME="/usr/share/sumo"
 export PATH="/usr/local/share/sumo/bin:/usr/local/share/sumo/tools:$PATH"
-export PATH="/opt/maven/bin:$PATH"
-export PATH="/opt/carma-simulation:$PATH"
+
