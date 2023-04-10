e47325ee8c9a3c33eeec68d874dfcf7be0d5ba9b
Cheng Yuan <cheng.yuan@leidos.com>
2023-04-10 15:07:40 -0400

    Correct copyright

--- co-simulation/fed/mosaic-infrastructure/src/main/java/org/eclipse/mosaic/fed/infrastructure/ambassador/InfrastructureMessageAmbassador.java
+++ co-simulation/fed/mosaic-infrastructure/src/main/java/org/eclipse/mosaic/fed/infrastructure/ambassador/InfrastructureMessageAmbassador.java
@@ -12,9 +12,8 @@
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
  * License for the specific language governing permissions and limitations under
  * the License.
- * 
  */
-
+ 
 package org.eclipse.mosaic.fed.infrastructure.ambassador;
 
 import org.eclipse.mosaic.rti.TIME;
