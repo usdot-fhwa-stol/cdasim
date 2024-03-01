009d5a106e39db81f6f52c3ee453ab9a3023ea73
Cheng Yuan <cheng.yuan@leidos.com>
2024-03-01 01:08:50 -0500

    Update carma-cloud integration scenarios

--- co-simulation/bundle/src/assembly/resources/scenarios/Town04_test/carla/bridge.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-#x-terminal-emulator
-cd /opt/carma-simulation/bridge
-
-x-terminal-emulator -e python3.7 carla_mosaic_bridge.py --bridge-server-port 8913 -m Town04 /opt/carma-simulation/scenarios/Town04_test/sumo/Town04.net.xml --step-length 0.1 --tls-manager EVC
