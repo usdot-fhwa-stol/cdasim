bcf120e8b3d3f54347f0ee4f993b051902a643d3
Cheng Yuan <cheng.yuan@leidos.com>
2024-03-01 10:39:41 -0500

    Fix sumo scenario issue

--- co-simulation/bundle/src/assembly/resources/scenarios/Town04_cloud_single_test/carla/bridge.sh
+++ co-simulation/bundle/src/assembly/resources/scenarios/Town04_cloud_single_test/carla/bridge.sh
@@ -3,4 +3,4 @@
 #x-terminal-emulator
 cd /opt/carma-simulation/bridge
 
-x-terminal-emulator -e python3.7 carla_mosaic_bridge.py --bridge-server-port 8913 -m Town04 /opt/carma-simulation/scenarios/Town04_cloud_test/sumo/Town04.net.xml --step-length 0.1 --tls-manager EVC
+x-terminal-emulator -e python3.7 carla_mosaic_bridge.py --bridge-server-port 8913 -m Town04 /opt/carma-simulation/scenarios/Town04_cloud_single_test/sumo/Town04.net.xml --step-length 0.1 --tls-manager EVC
