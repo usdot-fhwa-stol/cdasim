b42474d374b0b669d67925a4c34d20b6180c872c
Eric Chen <122404059+EricChen-Lei@users.noreply.github.com>
2024-11-05 13:25:28 -0500

    update port

--- co-simulation/fed/mosaic-carma-messenger/src/main/java/org/eclipse/mosaic/fed/carmamessenger/ambassador/CarmaMessengerBridgeRegistrationReceiver.java
+++ co-simulation/fed/mosaic-carma-messenger/src/main/java/org/eclipse/mosaic/fed/carmamessenger/ambassador/CarmaMessengerBridgeRegistrationReceiver.java
@@ -34,7 +34,7 @@ public class CarmaMessengerBridgeRegistrationReceiver implements Runnable{
 
     protected Queue<CarmaMessengerBridgeRegistrationMessage> rxQueue = new LinkedList<>();
     private DatagramSocket listenSocket = null;
-    private static final int listenPort = 1515;
+    private static final int listenPort = 1716;
     private boolean running = true;
     private static final int UDP_MTU = 1535;
     protected final Logger log = LoggerFactory.getLogger(this.getClass());
@@ -87,7 +87,7 @@ public class CarmaMessengerBridgeRegistrationReceiver implements Runnable{
             output.addAll(rxQueue);
             rxQueue.clear();
         }
-        
+
         return output;
     }
 }
