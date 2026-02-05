2d1c822520ee47d9ef4f239996aca989e30e587d
Eric <eric.chen@leidos.com>
2026-02-05 15:18:06 -0500

    remove ns3-2

--- co-simulation/lib/mosaic-docker/src/main/java/org/eclipse/mosaic/lib/docker/DockerClient.java
+++ co-simulation/lib/mosaic-docker/src/main/java/org/eclipse/mosaic/lib/docker/DockerClient.java
@@ -70,23 +70,10 @@ public class DockerClient {
 
     DockerContainer runImage(String image, String containerName, List<String> options, boolean removeBeforeRun) {
 
-        String dockerNetwork = System.getProperty("mosaic.docker.network");
-        if (StringUtils.isBlank(dockerNetwork)) {
-            dockerNetwork = System.getenv("MOSAIC_DOCKER_NETWORK");
-        }
-        logger.info("Resolved docker network: {}", dockerNetwork);
-
-        boolean isContainerNetwork = StringUtils.isNotBlank(dockerNetwork)
-                && dockerNetwork.startsWith("container:");
-
-        if (!options.contains("-p") && !options.contains("-P") && !isContainerNetwork) {
+        if(!options.contains("-p")){
             options.add("-P");
         }
 
-        if (StringUtils.isNotBlank(dockerNetwork) && !options.contains("--network")) {
-            options.add("--network");
-            options.add(dockerNetwork);
-        }
   
 
         // set name of container to default value if it hasn't been set
